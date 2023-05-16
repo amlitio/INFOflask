bfa2feaf5db5691468d1174bba28e885a8243ade
amlitio <gsiciaridis@gmail.com>
2023-05-16 12:45:22 -0400

    update features

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,12 @@
 from flask import Flask
-from app.models import SQLAlchemy
-#from app import SQLAlchemy
-#from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import importlib
-
-SQLAlchemy = importlib.import_module("app.models.SQLAlchemy")
-
+from app.models.SQLAlchemy import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
