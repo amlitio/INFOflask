899b33d7510719787caf9ac92492d9a89073009f
Gsic <gsiciaridis@gmail.com>
2023-05-16 12:00:59 -0400

    Your descriptive commit message here

--- app/models/folder.py
+++ app/models/folder.py
@@ -3,4 +3,5 @@ from app import db
 class Folder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # New field
     informations = db.relationship('Information', backref='folder', lazy='dynamic')
--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,9 @@ from werkzeug.security import check_password_hash
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)  # New field
     password = db.Column(db.String(128))
+    folders = db.relationship('Folder', backref='user', lazy='dynamic')  # New relationship
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
