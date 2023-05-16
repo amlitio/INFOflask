0fc050864b9c1088c68595cb3a2024953abf9abd
amlitio <gsiciaridis@gmail.com>
2023-05-16 12:46:19 -0400

    update features

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from app.models.SQLAlchemy import db
+from app.models import User  # assuming User is your user model
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
@@ -9,5 +10,9 @@ db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 from app.routes import index, login, signup, save, add_friend, collaborate, folder, ask_ai
